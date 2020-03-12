b60f1ca8a7b9283878f47b2ea0d588fe7297e9b5
Herberts Cruz <herbertscruz@gmail.com>
2020-03-12 20:04:19 -0300

    Initial project

---
+++ index.js
@@ -0,0 +1,91 @@
+
+const BancoDoBrasilValidator = require(
+  './lib/banco-do-brasil-validador',
+);
+// const BradescoValidator = require('./lib/bradesco-validator');
+// const ItauValidator = require('./lib/itau-validator');
+// const SantanderValidator = require('./lib/santander-validator');
+// const CitibankValidator = require('./lib/citibank-validator');
+// const HSBCValidator = require('./lib/hsbc-validator');
+// const BanrisulValidator = require('./lib/banrisul-validator');
+const GenericBankAccountValidator = require(
+  './lib/generic-bank-account-validator',
+);
+
+const Validator = function(bankId) {
+  const validators = {
+    '001': BancoDoBrasilValidator,
+    // '237': BradescoValidator,
+    // '341': ItauValidator,
+    // '033': SantanderValidator,
+    // '745': CitibankValidator,
+    // '399': HSBCValidator,
+    // '041': BanrisulValidator,
+  };
+
+  if (validators[bankId]) return validators[bankId];
+  return GenericBankAccountValidator;
+};
+
+module.exports = function validate(
+  bankId, branchNumber, branchCheckNumber,
+  accountNumber, accountCheckNumber,
+) {
+  const errors = [];
+  let description;
+  const genericValidator = new GenericBankAccountValidator();
+  const validator = new Validator(bankId);
+
+  if (!genericValidator.bankIdIsValid(bankId)) {
+    description = 'Invalid bank';
+    errors.push({description, code: 'INVALID_BANK_NUMBER'});
+  }
+
+  if (!validator.branchNumberIsValid(branchNumber)) {
+    description = validator.branchNumberMsgError();
+    errors.push({description, code: 'INVALID_BRANCH_NUMBER'});
+  }
+
+  if (!validator.branchCheckNumberIsValid(branchCheckNumber)) {
+    description = validator.branchCheckNumberMsgError();
+    errors.push({description, code: 'INVALID_BRANCH_CHECK_NUMBER'});
+  }
+
+  if (!validator.accountNumberIsValid(accountNumber)) {
+    description = validator.accountNumberMsgError();
+    errors.push({description, code: 'INVALID_ACCOUNT_NUMBER'});
+  }
+
+  if (!validator.accountCheckNumberIsValid(accountCheckNumber)) {
+    description = validator.accountCheckNumberMsgError();
+    errors.push({description, code: 'INVALID_ACCOUNT_CHECK_NUMBER'});
+  }
+
+  if (
+    validator.branchNumberIsValid(branchNumber) &&
+    validator.branchCheckNumberIsValid(branchCheckNumber)
+  ) {
+    if (!validator.branchCheckNumberMatch(branchNumber, branchCheckNumber)) {
+      description = 'The branch check number does not match ' +
+        'the completed branch number';
+      errors.push({description, code: 'BRANCH_CHECK_NUMBER_DONT_MATCH'});
+    }
+  }
+
+  if (
+    validator.accountNumberIsValid(accountNumber) &&
+    validator.accountCheckNumberIsValid(accountCheckNumber)
+  ) {
+    if (!validator.accountCheckNumberMatch(accountNumber, accountCheckNumber)) {
+      description = 'The account check number does not match ' +
+        'the completed branch/account number';
+      errors.push({description, code: 'ACCOUNT_CHECK_NUMBER_DONT_MATCH'});
+    }
+  }
+
+  // if (errors.length === 0) {
+  //   valid();
+  // } else {
+  //   invalid({errors: errors});
+  // }
+};
---
+++ test/example-ut-test.js
@@ -0,0 +1,13 @@
+
+const assert = require('chai').assert;
+
+/**
+ * Unit Tests
+ */
+describe('Example', () => {
+  describe('Test', () => {
+    it('should return ...', async () => {
+      assert.equal(0, 0);
+    });
+  });
+});
